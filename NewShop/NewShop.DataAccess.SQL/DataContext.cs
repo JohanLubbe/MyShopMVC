43f0f389af8ec260eafba519d2b8a205deba7036
Johan Lubbe <Lubbe.JPS@gmail.com>
2018-05-11 08:43:57 +0200

    After EF Process

---
+++ NewShop/NewShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using NewShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection") // Will then look in the webconfig in connection string section for the DefaultConnection
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
