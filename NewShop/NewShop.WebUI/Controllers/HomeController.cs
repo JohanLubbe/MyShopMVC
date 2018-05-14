324598b2eb4e167ab6d021ec19a82613d7fd9e70
Johan Lubbe <Lubbe.JPS@gmail.com>
2018-05-14 08:29:50 +0200

    ProductListing

--- NewShop/NewShop.WebUI/Controllers/HomeController.cs
+++ NewShop/NewShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NewShop.Core.Contracts;
+using NewShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,35 @@ namespace NewShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        // Create an instance of Products Repository
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        // Create a constructor
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            // Get a list of products
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
